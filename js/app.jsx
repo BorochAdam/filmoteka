c6f76b317ca73dbe637e69c254b31c0bbc736a47
BorochAdam <adamosdlugopis@gmail.com>
2018-11-22 19:25:27 +0100

    basic routing done

--- js/app.jsx
+++ js/app.jsx
@@ -8,12 +8,64 @@ import {
     NavLink,
 } from 'react-router-dom';
 
+class Navbar extends React.Component{
+    render(){
+        return(
+            <div>
+                <h1>filmoteka</h1>
+                <ul>
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/contact">Contact</Link>
+                    </li>
+                    <li>
+                        <Link to="/about">About me</Link>
+                    </li>
+                </ul>
+            </div>
+        )
+    }
+}
+
+class AboutMe extends React.Component{
+    render(){
+        return(
+            <div>
+                Hello, I'm Adam, nice to meet You :)
+            </div>
+        );
+    }
+}
+
+class Contact extends React.Component{
+    render(){
+        return(
+            <div>
+                Contact me at borochadam1991@gmail.com
+            </div>
+        );
+    }
+}
+
+class NotFound extends React.Component{
+    render(){
+        return(
+            <div>
+                Error 404 - Page not found.
+                <br/>
+                <Link to="/">Main page</Link>
+            </div>
+        )
+    }
+}
 
 class Main extends React.Component{
     render(){
         return(
             <div>
-                Loading... page
+                Main page
             </div>
         );
     }
@@ -22,7 +74,17 @@ class Main extends React.Component{
 class App extends React.Component{
     render(){
         return(
-            <Main/>
+            <HashRouter>
+                <div>
+                    <Navbar/>
+                    <Switch>
+                        <Route exact path="/" component={Main}/>
+                        <Route path="/contact" component={Contact}/>
+                        <Route path="/about" component={AboutMe}/>
+                        <Route component={NotFound}/>
+                    </Switch>
+                </div>
+            </HashRouter>
         );
     }
 }
